74e3923616892d47243d0e4619127903556a4170
wint3rmute <peteyridings@gmail.com>
2023-12-07 22:39:51 -0800

    Update setup-ftp.sh

--- File-Transfer/setup-ftp.sh
+++ File-Transfer/setup-ftp.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+sudo groupadd ftpgroup
+sudo useradd -g ftpgroup -d /dev/null -s /etc ftpuser
+sudo pure-pw useradd wintermute -u ftpuser -d /ftphome
+sudo pure-pw mkdb
+cd /etc/pure-ftpd/auth/
+sudo ln -s ../conf/PureDB 60pdb
+sudo mkdir -p /ftphome
+sudo chown -R ftpuser:ftpgroup /ftphome/
+sudo systemctl restart pure-ftpd
